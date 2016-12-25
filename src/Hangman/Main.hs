0c0b2e3e69bc11950cf87817f3208da2afc63d2b
Daniele Francesconi <dfrancesconi12@gmail.com>
2016-12-25 15:12:51 +0100

    refactoring

--- src/Hangman/Main.hs
+++ src/Hangman/Main.hs
@@ -6,40 +6,36 @@ import Data.Char
 import Data.List
 import System.Random
 
-data GameStatus = GameStatus { tries :: Int, playerWord :: String, hiddenWord :: String }
+data GameStatus = GameStatus { tries :: Int, triedLetters :: String, playerWord :: String, hiddenWord :: String }
 
 main :: IO ()
 main =
     do result <- try (readFile "words.txt") :: IO (Either SomeException String)
        case result of
          Left ex -> putStrLn $ "Caught exception: " ++ show ex
-         Right content -> startGame $ lines content
-
-
-startGame :: [String] -> IO ()
-startGame allWords = pickRandomWord allWords >>= \word -> runStateT loopGame (initNewGame word) >>= putStrLn . fst
-
-
-pickRandomWord :: [String] -> IO String
-pickRandomWord allWords = newStdGen >>= \r -> return $ allWords !! fst (randomR (0, length allWords - 1) r)
-
-
-initNewGame :: String -> GameStatus
-initNewGame word = GameStatus { tries = 2 * length (nub word), playerWord = [1..(length word)] *> "." , hiddenWord = word }
-
+         Right content ->
+           do r <- newStdGen
+              let allWords = lines content
+              let word = allWords !! fst (randomR (0, length allWords - 1) r)
+              let initialStatus = GameStatus { tries = 2 * length (nub word)
+                                               , triedLetters = []
+                                               , playerWord = [1..(length word)] *> "."
+                                               , hiddenWord = word }
+              runStateT loopGame initialStatus >>= putStrLn . fst
 
 loopGame :: StateT GameStatus IO String
 loopGame =
     do currentStatus <- get
        case currentStatus of
-         GameStatus t pw hw
+         GameStatus t tl pw hw
            | pw == hw  -> return $ "You won!!! :) The word is: " ++ show hw
            | t == 0    -> return $ "Sorry you have lost :( The word is: " ++ show hw
-           | otherwise -> do lift $ putStrLn $ "<< Left attempts " ++ show t ++ " -- " ++ show pw ++ " >>"
+           | otherwise -> do lift $ putStrLn $ "<< Left attempts " ++ show t ++ " -- " ++ show pw ++ " -- [ tried: " ++ tl ++ " ] >>"
                              guess <- lift gameInput
                              if length guess == 1 then
-                               if head guess `elem` pw then lift (putStrLn "You already have guessed this letter")
-                               else put (currentStatus { tries = t - 1, playerWord = guessedWord pw hw (head guess) })
+                               do let charGuessed = head guess
+                                  if charGuessed `elem` tl then lift (putStrLn "You already have tried this letter")
+                                  else put (currentStatus { tries = t - 1, triedLetters = sort $ nub (charGuessed:tl), playerWord = guessedWord pw hw charGuessed })
                              else put (currentStatus { tries = t - 1 })
                              loopGame
        where
